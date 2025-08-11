01291d4ab7c99ca3f40a972ce13d0d99f5d0b2bc
duquelf90 <duquekun@gmail.com>
2025-08-11 14:14:51 -0600

    update all

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -25,14 +25,14 @@ export default function ProductCard({ product }: Props) {
       <div className="bg-[#F0EEED] rounded-[13px] lg:rounded-[20px] w-full lg:max-w-[295px] aspect-square mb-2.5 xl:mb-4 overflow-hidden">
         <img
           src={product.images[0]}
-          alt={product.name}
+          alt={product.title}
           width={100}
           height={100}
           className="rounded-md w-full h-full object-contain hover:scale-110 transition-all duration-500"
         />
       </div>
-      <strong className="text-black xl:text-xl">{product.title}</strong>
-      <div className="flex items-center space-x-[5px] xl:space-x-2.5">
+      <strong className="text-black xl:text-lg">{product.title}</strong>
+      <div className="flex items-center w-full space-x-[5px] xl:space-x-2.5 mt-2 justify-between">
         <span className="font-bold text-black text-xl xl:text-2xl">
           ${product.price}
         </span>
